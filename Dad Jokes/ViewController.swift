f173573d871deca8282758d3f8cc2f4adf1a765b
Daniel Bolivar <dbolivarh@gmail.com>
2018-05-29 21:26:25 -0700

    Initial Commit

---
+++ Dad Jokes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dad Jokes
+//
+//  Created by Daniel Bolivar herrera on 29/05/2018.
+//  Copyright © 2018 Daniel Bolivar herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
