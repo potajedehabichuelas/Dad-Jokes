687e75a6f28031b74a3afebd4887588e2c76ce88
Daniel Bolivar <dbolivarh@gmail.com>
2018-05-30 14:04:19 -0700

    added joke autoupdate feature

--- Dad Jokes/ModelController/JokeModelController.swift
+++ Dad Jokes/ModelController/JokeModelController.swift
@@ -10,10 +10,51 @@ class JokeModelController {
 
     static let sharedInstance = JokeModelController()
     
-    private var jokes: [Joke] = DJStorage.retrieveJokes()
+    private var jokes: [Joke] = [] {
+        didSet {
+            self.saveJokes()
+        }
+    }
+    
+    private static let jokesRefillThreshold = 15
     
     var jokesCount: Int {
         return self.jokes.count
+    }
+    
+    func checkJokesStorageAmount() {
+        //Checks the amount of jokes stored and add some if required
+        if self.jokes.count < JokeModelController.jokesRefillThreshold {
+            self.addNewJokes { () in }
+        }
+    }
+    
+    func retrieveContacts(completion: @escaping () -> Void) {
+        //Retrieve Jokes
+        self.jokes = DJStorage.retrieveJokes()
+        //If we retrieved less than X jokes, we request a few more so we can be funny on time
+        if self.jokes.count <= JokeModelController.jokesRefillThreshold {
+            self.addNewJokes { () in
+                completion()
+            }
+        } else {
+            completion()
+        }
+    }
+    
+    private func addNewJokes(completion: @escaping () -> Void) {
+        var jokesReceived = 0
+        //This ideally would be achieved by requesting a batch of jokes, but server doesn't support pagination ?
+        for _ in 0..<JokeModelController.jokesRefillThreshold {
+            JokeRequestManager.sharedInstance.queryDadJoke(completion: { joke in
+                guard let id = joke.id, let jokeText = joke.joke, let link = joke.permalink else { return }
+                self.createJoke(id: id, text: jokeText, permaLink: link)
+                jokesReceived += 1
+                if jokesReceived == JokeModelController.jokesRefillThreshold {
+                    completion()
+                }
+            })
+        }
     }
     
     func createJoke(id: String, text: String, permaLink: String) {
