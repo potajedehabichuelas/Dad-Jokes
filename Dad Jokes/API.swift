b2f8dbeefa0fd0caa693a9e54a0884e31131aab0
Daniel Bolivar <dbolivarh@gmail.com>
2018-05-30 09:53:59 -0700

    Updated GraphQL request model

--- Dad Jokes/API.swift
+++ Dad Jokes/API.swift
@@ -2,9 +2,11 @@
 
 import Apollo
 
-public final class JokeDetailsQuery: GraphQLQuery {
+public final class DadJokeQuery: GraphQLQuery {
   public static let operationString =
-    "query JokeDetails {\n  joke {\n    __typename\n    id\n    joke\n    permalink\n  }\n}"
+    "query DadJoke {\n  joke {\n    __typename\n    ...JokeDetails\n  }\n}"
+
+  public static var requestString: String { return operationString.appending(JokeDetails.fragmentString) }
 
   public init() {
   }
@@ -39,6 +41,7 @@ public final class JokeDetailsQuery: GraphQLQuery {
       public static let possibleTypes = ["Joke"]
 
       public static let selections: [GraphQLSelection] = [
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
         GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
         GraphQLField("id", type: .scalar(GraphQLID.self)),
         GraphQLField("joke", type: .scalar(String.self)),
@@ -90,6 +93,88 @@ public final class JokeDetailsQuery: GraphQLQuery {
           snapshot.updateValue(newValue, forKey: "permalink")
         }
       }
+
+      public var fragments: Fragments {
+        get {
+          return Fragments(snapshot: snapshot)
+        }
+        set {
+          snapshot += newValue.snapshot
+        }
+      }
+
+      public struct Fragments {
+        public var snapshot: Snapshot
+
+        public var jokeDetails: JokeDetails {
+          get {
+            return JokeDetails(snapshot: snapshot)
+          }
+          set {
+            snapshot += newValue.snapshot
+          }
+        }
+      }
+    }
+  }
+}
+
+public struct JokeDetails: GraphQLFragment {
+  public static let fragmentString =
+    "fragment JokeDetails on Joke {\n  __typename\n  id\n  joke\n  permalink\n}"
+
+  public static let possibleTypes = ["Joke"]
+
+  public static let selections: [GraphQLSelection] = [
+    GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+    GraphQLField("id", type: .scalar(GraphQLID.self)),
+    GraphQLField("joke", type: .scalar(String.self)),
+    GraphQLField("permalink", type: .scalar(String.self)),
+  ]
+
+  public var snapshot: Snapshot
+
+  public init(snapshot: Snapshot) {
+    self.snapshot = snapshot
+  }
+
+  public init(id: GraphQLID? = nil, joke: String? = nil, permalink: String? = nil) {
+    self.init(snapshot: ["__typename": "Joke", "id": id, "joke": joke, "permalink": permalink])
+  }
+
+  public var __typename: String {
+    get {
+      return snapshot["__typename"]! as! String
+    }
+    set {
+      snapshot.updateValue(newValue, forKey: "__typename")
+    }
+  }
+
+  public var id: GraphQLID? {
+    get {
+      return snapshot["id"] as? GraphQLID
+    }
+    set {
+      snapshot.updateValue(newValue, forKey: "id")
+    }
+  }
+
+  public var joke: String? {
+    get {
+      return snapshot["joke"] as? String
+    }
+    set {
+      snapshot.updateValue(newValue, forKey: "joke")
+    }
+  }
+
+  public var permalink: String? {
+    get {
+      return snapshot["permalink"] as? String
+    }
+    set {
+      snapshot.updateValue(newValue, forKey: "permalink")
     }
   }
 }
