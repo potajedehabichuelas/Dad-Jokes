18cae398f480bc8d76b2bb96fb7c4b64b1d493d6
Daniel Bolivar <dbolivarh@gmail.com>
2018-05-30 13:10:06 -0700

    Updated pods

--- Podfile
+++ Podfile
@@ -5,6 +5,8 @@ target 'Dad Jokes' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'Apollo'
+  pod 'NVActivityIndicatorView'
+  pod 'SwiftMessages'
 
   # Pods for Dad Jokes
 
--- Pods/Target Support Files/Pods-Dad Jokes/Pods-Dad Jokes-frameworks.sh
+++ Pods/Target Support Files/Pods-Dad Jokes/Pods-Dad Jokes-frameworks.sh
@@ -144,9 +144,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Apollo/Apollo.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NVActivityIndicatorView/NVActivityIndicatorView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages/SwiftMessages.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Apollo/Apollo.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NVActivityIndicatorView/NVActivityIndicatorView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftMessages/SwiftMessages.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
